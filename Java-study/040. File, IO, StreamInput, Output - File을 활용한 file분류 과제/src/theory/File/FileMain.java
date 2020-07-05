48d5082e8cb8006243bb4f98ca94ef2c9525b698
박성언 <tjddjs90@naver.com>
2020-07-05 14:57:35 +0900

    040. File, IO, StreamInput, Output - File을 활용한 file분류 과제

--- Java-study/040. File, IO, StreamInput, Output - File을 활용한 file분류 과제/src/theory/File/FileMain.java
+++ Java-study/040. File, IO, StreamInput, Output - File을 활용한 file분류 과제/src/theory/File/FileMain.java
@@ -17,31 +17,34 @@ public class FileMain {
 //		File ff = new File("예제폴더/exam1.txt");
 		File ff = new File("./예제폴더");
 
-		System.out.println("존재유무(boolean) : " + ff.exists());
-		System.out.println("파일인지(boolean) : " + ff.isFile());
-		System.out.println("폴더인지(boolean) : " + ff.isDirectory());
-		System.out.println("절대경로인지(boolean) : " + ff.isAbsolute());
-		System.out.println("숨김파일인지(boolean) : " + ff.isHidden());
+		System.out.println("존재유무(boolean) : " + ff.exists()); // true
+		System.out.println("파일인지(boolean) : " + ff.isFile()); // false
+		System.out.println("폴더인지(boolean) : " + ff.isDirectory()); // true
+		System.out.println("절대경로인지(boolean) : " + ff.isAbsolute()); // false
+		System.out.println("숨김파일인지(boolean) : " + ff.isHidden()); // false
 
-		System.out.println("부모폴더(String) : " + ff.getParent());
-		System.out.println(ff.getParent() instanceof String);
-		System.out.println("파일명(String) : " + ff.getName());
-		System.out.println(ff.getName() instanceof String);
+		System.out.println("부모폴더(String) : " + ff.getParent()); // . => 없다는 의미같습니다. 최상위(?)
+		System.out.println(ff.getParent() instanceof String); // true
+
+		System.out.println("파일명(String) : " + ff.getName()); // 예제폴더
+		System.out.println(ff.getName() instanceof String); // true
 
 		System.out.println("절대경로(String) : " + ff.getAbsolutePath());
-		System.out.println(ff.getAbsolutePath() instanceof String);
+		// D:\Study\Java\Java-study\040. File, IO, StreamInput, Output - File을 활용한
+		// file분류 과제\.\예제폴더
+		System.out.println(ff.getAbsolutePath() instanceof String); // true
 
-		System.out.println("경로(String) : " + ff.getPath());
-		System.out.println(ff.getPath() instanceof String);
+		System.out.println("경로(String) : " + ff.getPath()); // .\예제폴더
+		System.out.println(ff.getPath() instanceof String); // true
 
-		System.out.println("실행유무(boolean) : " + ff.canExecute());
-		System.out.println("읽기유무(boolean) : " + ff.canRead());
-		System.out.println("쓰기유무(boolean) : " + ff.canWrite());
+		System.out.println("실행유무(boolean) : " + ff.canExecute()); // true
+		System.out.println("읽기유무(boolean) : " + ff.canRead()); // true
+		System.out.println("쓰기유무(boolean) : " + ff.canWrite()); // true
 
 		System.out.println("파일크기 : " + ff.length()); // byte 기준 적어도 1byte는 잡혀야 합니다...
 
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:ss:mm");
-		System.out.println("마지막수정(Date) : " + sdf.format(new Date(ff.lastModified())));
+		System.out.println("마지막수정(Date) : " + sdf.format(new Date(ff.lastModified()))); // 14:01:51
 
 		System.out.println();
 		System.out.println("-----------------------------------------------------------------");
@@ -64,6 +67,7 @@ public class FileMain {
 		deleteFile.delete();
 
 		System.out.println();
+		System.out.println("-----------------------------------------------------------------");
 		System.out.println("피일생성");
 		try {
 
@@ -78,6 +82,7 @@ public class FileMain {
 		}
 
 		System.out.println();
+		System.out.println("-----------------------------------------------------------------");
 		System.out.println("폴더생성");
 		File newDirectory = new File("새로운 폴더 생성");
 		newDirectory.mkdir();
@@ -93,6 +98,7 @@ public class FileMain {
 		}
 
 		System.out.println();
+		System.out.println("-----------------------------------------------------------------");
 		System.out.println("다중 폴더 생성");
 		File newMultiDirectory = new File("./다중폴더(outer)/다중폴더(inner)");
 		newMultiDirectory.mkdirs();
@@ -118,17 +124,26 @@ public class FileMain {
 		}
 
 		System.out.println();
+		System.out.println("-----------------------------------------------------------------");
 		System.out.println("File들을 배열화 => listFiles");
 		System.out.println();
 
 		File file = new File("./예제폴더");
 		File[] fileTree = file.listFiles();
 		System.out.println(Arrays.toString(fileTree));
+		// [.\예제폴더\CreateFileTest.txt, .\예제폴더\exam1.txt, .\예제폴더\exam2.txt,
+		// .\예제폴더\exam3.txt, .\예제폴더\exam4.abc, .\예제폴더\stud.csv]
 		System.out.println();
 
 		for (File file2 : fileTree) {
 			System.out.println(file2.getName());
 		}
+//		CreateFileTest.txt
+//		exam1.txt
+//		exam2.txt
+//		exam3.txt
+//		exam4.abc
+//		stud.csv
 	}
 
 }
