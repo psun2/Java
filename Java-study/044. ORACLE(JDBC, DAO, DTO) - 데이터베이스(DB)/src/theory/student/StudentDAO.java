e2c4a2d51cff1f81e9a3783a10b4d8fd6f17f359
박성언 <tjddjs90@naver.com>
2020-07-12 03:37:25 +0900

    database

--- Java-study/044. ORACLE(JDBC, DAO, DTO) - 데이터베이스(DB)/src/theory/student/StudentDAO.java
+++ Java-study/044. ORACLE(JDBC, DAO, DTO) - 데이터베이스(DB)/src/theory/student/StudentDAO.java
@@ -11,14 +11,14 @@ public class StudentDAO {
 	Connection con;
 	Statement stmt;
 	ResultSet rs;
-	String url, user, password;
+	String url, user, password, sql;
 
 	public StudentDAO() {
 		// TODO Auto-generated constructor stub
 		this.url = "jdbc:oracle:thin:@localhost1521:xe";
 		this.user = "hr";
 		this.password = user;
-		
+
 		try {
 			this.con = DriverManager.getConnection(url, user, password);
 			this.stmt = con.createStatement();
