03222c132a129eae63c4cb689fb55fd59637fb30
project <project@naver.com>
2020-07-29 14:30:09 +0900

    pro

--- Java-study/Korea_Java_MiniProject_PuyoPuyo/src/ddong/InitData.java
+++ Java-study/Korea_Java_MiniProject_PuyoPuyo/src/ddong/InitData.java
@@ -1,9 +1,10 @@
 package ddong;
 
 public class InitData {
-	
-	public static String ip = "172.30.1.14";
-	
+
+//	public static String ip = "172.30.1.14";
+	public static String ip = "192.168.0.117";
+
 	public static String id = "hr", pw = "hr";
 
 }
