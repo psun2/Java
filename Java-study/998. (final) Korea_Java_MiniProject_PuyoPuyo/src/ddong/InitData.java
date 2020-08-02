cb400952b5499021372b51b6e36de73eb86414b3
박성언 <tjddjs90@naver.com>
2020-08-03 00:24:24 +0900

    pro

--- Java-study/998. (final) Korea_Java_MiniProject_PuyoPuyo/src/ddong/InitData.java
+++ Java-study/998. (final) Korea_Java_MiniProject_PuyoPuyo/src/ddong/InitData.java
@@ -2,7 +2,8 @@ package ddong;
 
 public class InitData {
 
-	public static String ip = "192.168.1.9";
+//	public static String ip = "192.168.1.9";
+	public static String ip = "172.30.1.48";
 	public static String id = "hr", pw = "hr";
 
 }
--- Java-study/998. (final) Korea_Java_MiniProject_PuyoPuyo/src/game_p/PuyoFrame.java
+++ Java-study/998. (final) Korea_Java_MiniProject_PuyoPuyo/src/game_p/PuyoFrame.java
@@ -268,9 +268,5 @@ public class PuyoFrame extends JFrame implements DDongInter, WindowListener {
 		// TODO Auto-generated method stub
 
 	}
-	
-	public static void main(String[] args) {
-		new PuyoFrame(12, "hjk", "asdasd");
-	}
 
 }
