d3c22c64923f832f43a51854e3b6449248979647
박성언 <tjddjs90@gmail.com>
2020-08-03 00:35:36 +0900

    pro final

--- Java-study/998. (final) Korea_Java_MiniProject_PuyoPuyo/src/ddong/InitData.java
+++ Java-study/998. (final) Korea_Java_MiniProject_PuyoPuyo/src/ddong/InitData.java
@@ -3,7 +3,8 @@ package ddong;
 public class InitData {
 
 //	public static String ip = "192.168.1.9";
-	public static String ip = "172.30.1.48";
+//	public static String ip = "172.30.1.48";
+	public static String ip = "192.168.0.15";
 	public static String id = "hr", pw = "hr";
 
 }
