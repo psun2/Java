90665b703bcf9780125ba2c67b8d463c40743fb6
박성언 <tjddjs90@gmail.com>
2020-08-04 23:42:58 +0900

    동빈나 채팅

--- Java-study/043. NetWork 동빈_Chat Client - 채팅프로그램(FX 이용)/src/application/Main.java
+++ Java-study/043. NetWork 동빈_Chat Client - 채팅프로그램(FX 이용)/src/application/Main.java
@@ -7,7 +7,14 @@ import java.net.Socket;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -97,7 +104,114 @@ public class Main extends Application {
 	// 실제로 프로그램을 동작시키는 메소드입니다.
 	@Override
 	public void start(Stage primaryStage) {
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(5));
 
+		HBox hbox = new HBox();
+		hbox.setSpacing(5);
+
+		TextField userName = new TextField();
+		userName.setPrefWidth(150);
+		userName.setPromptText("닉네임을 입력하세요.");
+
+		// HBox 내부에서 해당 TextFieled 가
+		// 출력이 될수 있도록 ....
+		HBox.setHgrow(userName, Priority.ALWAYS);
+
+		// 기본적으로 사용자 자신의 IP가 들어 갈 수 있게 해 줍니다.
+		TextField IPText = new TextField("127.0.0.1");
+
+		TextField portText = new TextField("7777");
+		portText.setPrefWidth(80);
+
+		// 실질적으로 hbox 내부에 3개의 텍스트 필드가 추가 될 수 있도록 해줍니다.
+		hbox.getChildren().addAll(userName, IPText, portText);
+
+		// hbox 를 윗쪽에 배치
+		root.setTop(hbox);
+		
+		textArea = new TextArea();
+
+		// 내용수정 불가
+		textArea.setEditable(false);
+
+		// 레이아웃의 중간에 textArea 배치
+		root.setCenter(textArea);
+
+		TextField input = new TextField();
+		input.setPrefWidth(Double.MAX_VALUE);
+
+		// 접속하기 이전에는 메세지 전송을 불가 하게 만들어 줍니다.
+		input.setDisable(true);
+
+		input.setOnAction(event -> {
+			send(userName.getText() + ": " + input.getText() + "\n");
+			input.setText("");
+
+			// 보낸후 다시 보낼수 있도록 포커스를 재배치
+			input.requestFocus();
+		});
+
+		Button sendButton = new Button("보내기");
+
+		// 접속하기 이전엔 비활 성화
+		sendButton.setDisable(true);
+
+		sendButton.setOnAction(event -> {
+			send(userName.getText() + ": " + input.getText() + "\n");
+			input.setText("");
+
+			// 보낸후 다시 보낼수 있도록 포커스를 재배치
+			input.requestFocus();
+		});
+
+		Button connectionButton = new Button("접속하기");
+		connectionButton.setOnAction(event -> {
+			if (connectionButton.getText().equals("접속하기")) {
+				// 기본적인 포트번호가 설정되어 있고
+				// 사용자가 다른 포트를 설정한다면 변경되어
+				// 해당 포트에 연결할 수 있도록 합니다.
+				int port = 7777;
+				try {
+					port = Integer.parseInt(portText.getText());
+				} catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+				startClient(IPText.getText(), port);
+				Platform.runLater(() -> {
+					textArea.appendText("[ 채팅방 접속 ]\n");
+				});
+				connectionButton.setText("종료하기");
+				input.setDisable(false);
+				sendButton.setDisable(false);
+				input.requestFocus();
+			} else {
+				stopClient();
+				Platform.runLater(() -> {
+					textArea.appendText("[ 채팅방 퇴장 ]\n");
+				});
+				connectionButton.setText("접속하기");
+				input.setDisable(true);
+				sendButton.setDisable(true);
+				input.requestFocus();
+			}
+		});
+
+		BorderPane pane = new BorderPane();
+		pane.setLeft(connectionButton);
+		pane.setCenter(input);
+		pane.setRight(sendButton);
+
+		root.setBottom(pane);
+		Scene scene = new Scene(root, 400, 400);
+
+		primaryStage.setTitle("[ 채팅 클라이언트 ]");
+		primaryStage.setScene(scene);
+		primaryStage.setOnCloseRequest(event -> stopClient());
+		primaryStage.show(); // 보여지도록 만듬
+		
+		connectionButton.requestFocus();
 	}
 
 	// 프로그램의 진입점입니다.
