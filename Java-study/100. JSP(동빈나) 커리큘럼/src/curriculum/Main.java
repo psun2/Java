da46c3673bbc5c100ad867e7b262cc16cd5f8816
박성언 <tjddjs90@gmail.com>
2020-08-18 15:13:52 +0900

    동빈나 커리큘럼 업데이트

---
+++ Java-study/100. JSP(동빈나) 커리큘럼/src/curriculum/Main.java
@@ -0,0 +1,50 @@
+package curriculum;
+
+public class Main {
+
+	// JSP 강의평가 웹 사이트 개발하기
+	// https://www.youtube.com/watch?v=hke9FKluXow&list=PLRx0vPvlEmdAdWCQeUPnyMZ4PsW3dGuFB&index=1
+
+//--------------------------------------------------------------------------------------------------------------	
+
+	// ajax 익명 채팅사이트 개발
+	// https://www.youtube.com/watch?v=_eC_lbmoizg&list=PLRx0vPvlEmdAVcSdYgqjJ64A7ggHhorU_
+
+//--------------------------------------------------------------------------------------------------------------	
+
+	// JSP Ajax 회원가입 기능 구현하기 1강 - 프로젝트 소개 및 회원가입 화면 디자인
+	// https://www.youtube.com/watch?v=XxeLEMC5jzA&t=11s
+
+	// JSP Ajax 회원가입 기능 구현하기 2강 - 데이터베이스 구축 및 연동
+	// https://www.youtube.com/watch?v=Qda2BovSIQ0
+
+	// JSP Ajax 회원가입 기능 구현하기 3강 - 회원 가입 기능 구현하기
+	// https://www.youtube.com/watch?v=2sf1gtSAXMo
+
+	// JSP Ajax 회원가입 기능 구현하기 4강 - 사용자 친화적인 메시지 알림창 구현하기
+	// https://www.youtube.com/watch?v=4q3r1uIhc9U
+
+	// JSP Ajax 회원가입 기능 구현하기 5강 - 회원가입 중복 체크 및 프로젝트 완료
+	// https://www.youtube.com/watch?v=PGXqDZPqlWo
+
+//--------------------------------------------------------------------------------------------------------------	
+
+	// 나만의 알고리즘 채점 사이트 만들기
+	// https://www.youtube.com/watch?v=hYr8ZHv40Nk&list=PLRx0vPvlEmdCSBgU29ZEmxqTr4ge-MoN_
+
+//--------------------------------------------------------------------------------------------------------------	
+
+	// JSP 파일 업로드 강좌
+	// https://www.youtube.com/watch?v=UQVyytDtLzQ&list=PLRx0vPvlEmdBMeD6NZawEmwHKjzD6ZGqp
+
+//--------------------------------------------------------------------------------------------------------------	
+
+	// JSP 게시판 만들기 강좌
+	// https://www.youtube.com/watch?v=wEIBDHfoMBg&list=PLRx0vPvlEmdAZv_okJzox5wj2gG_fNh_6
+
+//--------------------------------------------------------------------------------------------------------------	
+
+	// JSP Ajax 고급 회원제 채팅 사이트 개발하기
+	// https://www.youtube.com/watch?v=88eNrWzLk-A&list=PLRx0vPvlEmdAlUbX_TGDxaSxKCvfl2isa
+
+}
