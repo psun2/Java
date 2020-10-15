2a2afe53c9effeab72735a28b911b15035f3d08d
박성언 <tjddjs90@naver.com>
2020-10-15 12:00:55 +0900

    정적파일, 동적파일, get방식의 parameter

--- Java-study/302. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 김영한/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Java-study/302. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 김영한/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,18 +3,28 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    // url 이 /hello 로 들어오면
-    // 맵핑된 메소드를 호출 합니다.
     @GetMapping("hello")
+    // 주소 맵핑
+    // ex) localhost:8080/hello
     public String hello(Model model) {
+        model.addAttribute("data", "Spring!!");
+        return "hello";
+    }
 
-        // 자료를 넘깁니다.
-        model.addAttribute("data", "hello!!");
-
-        return "hello"; // viewName (파일 이름) hello.html
+    @GetMapping("hello-mvc")
+    // 주소 맵핑
+    // ex) localhost:8080/hello-mvc
+    // public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
+    // @RequestParam("name") get방식
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        // required default 가 true 이며 default 즉 true 상태 일때는 무조건
+        // parameter을 넘겨야합니다.
+        model.addAttribute("name", name);
+        return "hello-template"; // templates의 실행할 파일 명
     }
 }
