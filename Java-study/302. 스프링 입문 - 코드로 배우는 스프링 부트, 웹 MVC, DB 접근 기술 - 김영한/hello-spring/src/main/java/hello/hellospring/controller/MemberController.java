b9b6bf0a9786fbe52479948321ac4337fad6aa75
박성언 <tjddjs90@naver.com>
2020-10-18 11:35:40 +0900

    > 2020-10-18 | 회원 웹 기능 - 등록

--- Java-study/302. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 김영한/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Java-study/302. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 김영한/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 // @Controller 라는 애노테이션을 보고
@@ -22,5 +25,33 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new") // urlMapping
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new") // Param Mapping
+    public String create (MemberForm form) {
+        // 들여오는 MemberForm 에 있는 필드 명은 html 의 name 명이랑 같아야
+        // spring 이 자동적으로 이름에 맞는 필드를 매칭 시켜 set~ 을 시켜줍니다.
+        // 그럼 form 이라는 우리가 흔히 아는 데이터가 setting 된 DTO 를 만들어 주고
+
+        // 우리는 이제 form 에서 값을 꺼내어 유효성 검사를 한뒤
+        
+        Member member = new Member();
+        member.setName(form.getName());
+
+        // Member 라는 객체를 생성 하여 각 필드에 입력 해줍니다.
+
+        // 굳이 똑같은 파일을 가르는 이유가 있을까... ?
+        // 이 예제에서는 id 라는 필드를 primary key  로 사용 하기 위하여
+        // id 값은 받고 있지 않아서 ... ? (아직은 잘 모르겠다...)
+
+        memberService.join(member);
+        // 모든 유효성이 통과 되었으므로 db에 저장을 한뒤
+
+        return "redirect:/"; // 홈화면(root) 로 돌아가기
     }
 }
---
+++ Java-study/JSP07_ServletContext  (Context 의 XML 및 Path)/WebContent/index.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<title>Insert title here</title>
+</head>
+<body>
+		<div>
+			<a href="home.jsp">홈</a>
+			&nbsp;
+			<strong>http://localhost:8080/z_test_context/home.jsp</strong>
+			</div>
+		<div>
+			<a href="./home.jsp">./홈</a>
+			&nbsp;
+			<strong>http://localhost:8080/z_test_context/home.jsp</strong>
+		</div>
+		<div>
+			<a href="/home.jsp">/홈</a>
+			&nbsp;
+			<strong>http://localhost:8080/home.jsp</strong>
+		</div>
+		
+</body>
+</html>
