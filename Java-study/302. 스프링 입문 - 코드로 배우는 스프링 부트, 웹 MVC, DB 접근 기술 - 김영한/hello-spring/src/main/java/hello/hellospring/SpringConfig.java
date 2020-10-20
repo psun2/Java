00efdf5fa0254ab309d2ff0ca17cae2cbc02eb94
박성언 <tjddjs90@naver.com>
2020-10-20 10:22:16 +0900

    JDBC

--- Java-study/302. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 김영한/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Java-study/302. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 김영한/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,9 +1,15 @@
-package hello.hellospring.service;
+package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 이전 시간 @AutoWired 와
 // @Repository, @Service 로 자동으로 spring bean 에 등록하여 썻다면,
@@ -12,6 +18,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean // 스프링 IOC 컨테이너에 등록 합니다.
     // 장점 코드 수정이 없고 바꿔 끼울 객체의 명만 변경 해주면 됩니다.
@@ -26,6 +39,8 @@ public class SpringConfig {
     @Bean // 스프링 IOC 컨테이너에 등록 합니다.
     // 장점 코드 수정이 없고 바꿔 끼울 객체의 명만 변경 해주면 됩니다.
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
--- Java-study/302. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 김영한/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ Java-study/302. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 김영한/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -2,32 +2,23 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class MemberServiceTest {
-
+@SpringBootTest
+@Transactional // test가 끝나면 rollback 할 수 있습니다.
+//@SpringBootTest : 스프링 컨테이너와 테스트를 함께 실행한다.
+//@Transactional : 테스트 케이스에 이 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고, 테스
+//        트 완료 후에 항상 롤백한다. 이렇게 하면 DB에 데이터가 남지 않으므로 다음 테스트에 영향을 주지 않는다.
+public class MemberServiceIntegrationTest {
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-        // 외부에서 주입 받는다 => Dependency Injection
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     void 회원가입() { // 테스트 코드를 작성할때 제약이 없다면
@@ -70,13 +61,5 @@ class MemberServiceTest {
 //        }
 
         // then
-    }
-
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
     }
 }
